f8756436309fff30777682b9ca756bc3255519d2
shofi384@gmail.com Rahman <shofi384@gmail.com>
2018-07-16 15:12:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,15 +14,4 @@ const codes = [
 let index = 0;
 
 function init() {
-  document.body.addEventListener('keydown', function(event) {
-    if(event.key === codes[index]) {
-      index++
-      if(index === codes.length) {
-        alert('Congratulations!')
-        index = 0
-      }
-    }
-    else
-    index = 0
-  });
 }
